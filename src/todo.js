470b0f6ff7028411ced63285b1568c487dd6421e
Guber <orga.gbr@posteo.de>
2020-04-15 15:53:35 +0200

    Implement modal-opening with fetched data - id safe

--- src/todo.js
+++ src/todo.js
@@ -2,16 +2,27 @@ const todoWrapper = document.querySelector('.content-wrapper');
 const allCheckBoxContainer =
     todoWrapper.getElementsByTagName("input");
 const addTodoButton = document.querySelector('.add-todo');
-const todoItem = document.querySelector('.todo-box');
+
+const todoItems = document.getElementsByClassName('todo-box');
+
 const todoAllSpans = todoWrapper.querySelectorAll('span');
-const todoCheckSpan = todoAllSpans[1];
 const toggleWrapper = document.querySelector('.checkbox-container');
-const todoText = document.querySelector('textarea');
+const todoText = document.getElementsByClassName('todo-text');
+console.log('todotext');
 const todoDeadline = document.querySelector('.due_date_ticker').children;
 const todoDate = todoDeadline[0];
 const todoTime = todoDeadline[1];
 const deleteBtn = document.querySelector('.todo-delete');
 const saveBtn = document.querySelector('.todo-save');
+
+//Select elements in modal
+const modal = document.querySelector('.modal-container');
+const modalTextarea = modal.querySelector('textarea');
+console.log(modalTextarea);
+const modalDate = modal.querySelector('#todoDate');
+const modalTime = modal.querySelector('#todoTime');
+console.log(modalDate);
+console.log(modalTime);
 
 let todoDataBase = [];
 let uuid = 0;
@@ -37,6 +48,9 @@ const createTodoItem = (id, todoTaskTitle, isDone, doUntilDate, doUntilTime) => 
     const isStrike = isDone ? strikeTodo : openTodo;
     // const isDoneSetGrey = isDone ? grey : openTodo;
 
+    // console.log('true if not empty');
+    // console.log(doUntilDate === true);
+
     // todo: Dummy Date
     const dateConfig = {weekday: 'long', month: 'long', day: 'numeric'};
     const doneDeadlineDate = new Date(doUntilDate).toLocaleDateString('en-US', dateConfig);
@@ -51,7 +65,7 @@ const createTodoItem = (id, todoTaskTitle, isDone, doUntilDate, doUntilTime) => 
                             <span class="material-icons trash-icon" id="${id}">delete</span> Delete
                         </div>
                         <label class="checkbox-container">
-                            <div class="todo-text">
+                            <div class="todo-text" id="${id}">
                                 ${todoTaskTitle}
                             </div> 
                                 <input type="checkbox" id="${id}">
@@ -84,11 +98,10 @@ if (fetchedTodos) {
 }
 
 const saveTodoItem = () => {
-    const todoTextContent = todoText.value;
+    const todoTextContent = modalTextarea.value;
     const todoUntilDate = todoDate.value;
     const todoUntilTime = todoTime.value;
 
-    // Validation
 
     createTodoItem(uuid, todoTextContent, false, todoUntilDate, todoUntilTime);
     // console.log(`log from saveTodoItem:  ${uuid} ${todoTextContent} ${todoUntilDate} ${todoUntilTime}`);
@@ -123,9 +136,10 @@ const toggleTodoTask = (todoItem) => {
 
 
 [...allCheckBoxContainer].forEach(inputTag => inputTag.nextSibling.nextSibling.addEventListener('click', event => {
+    // event.stopPropagation();
+    // event.stopImmediatePropagation();
     console.log('element that clicked');
     console.log(event.currentTarget);
-    // const circle = event.target;
     const circle = event.target;
     console.log('circle');
     console.log(circle);
@@ -134,9 +148,9 @@ const toggleTodoTask = (todoItem) => {
 }));
 
 const deleteTodo = (id) => {
-    console.log(todoDataBase.map(item => item.id));
+    // console.log(todoDataBase.map(item => item.id));
     console.log(typeof id);
-    console.log(typeof todoDataBase[0].id);
+    // console.log(typeof todoDataBase[0].id);
     const newDB = todoDataBase.filter(item => item.id.toString() !== id);
     todoDataBase = newDB;
     // let db = todoDataBase.filter(item => item.id !== id);
@@ -146,16 +160,47 @@ const deleteTodo = (id) => {
 };
 
 
-
 const deleteIcons = document.querySelectorAll('.trash-icon');
-console.log(deleteIcons);
-
 [...deleteIcons].forEach(trashItem => trashItem.addEventListener('click', event => {
+    // event.stopPropagation();
     const trash = event.currentTarget;
-    console.log(trash);
-    console.log(trash.id);
+    // console.log(trash);
+    // console.log(trash.id);
     deleteTodo(trash.id);
+}));
+
+// Edit todoitem functionality
+const showFetchedModal = (id) => {
+    showModal();
+
+    const todoContent = todoDataBase.filter(item => id === item.id.toString());
+    todoDataBase.map(tasks => console.log(tasks));
+    modalTextarea.textContent = todoContent[0].task;
+    modalDate.textContent = todoContent[0].doUntilDate;
+    modalTime.textContent = todoContent[0].doUntilTime;
+
+
+    // const todoTextContent = modalTextarea.value;
+    // const todoUntilDate = modalDate.value;
+    // const todoUntilTime = modalTime.value;
+    //
+    // todoDataBase[id].task = todoTextContent;
+    // todoDataBase[id].doUntilDate = todoUntilDate;
+    // todoDataBase[id].doUntilTime = todoUntilTime;
+    //
+    // localStorage.setItem('todo_items', JSON.stringify(todoDataBase));
+
+};
+
+
+[...todoText].forEach(item => item.addEventListener('click', event => {
+    showModal();
+    console.log('clickes on for modal');
+    console.log(event.target.id);
+    showFetchedModal(event.target.id);
 }));
 
 saveBtn.addEventListener('click', saveTodoItem);
 
+console.log('todoItems');
+console.log(todoItems);
